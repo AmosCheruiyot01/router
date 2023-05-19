2cc4f8fd9f4c62c55288da8606ac16eef6c81bde
AmosCheruiyot01 <cheruiyotamos728@gmail.com>
2023-05-19 11:39:36 +0300

    6th commit

--- src/pages/not.jsx
+++ src/pages/not.jsx
@@ -2,6 +2,7 @@ const NotFound = () =>{
     return(
         <div className="error">
             <h5>error! page not found</h5>
+            <Link className="shortcuts" to="/">Go back to home</Link>
         </div>
     );
 }
